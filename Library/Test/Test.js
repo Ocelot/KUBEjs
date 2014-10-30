5568164c3d0d75ed1f15cd724a55f33208adfd30
Ian Reid <ian@redscotch.com>
2014-10-30 10:07:46 -0700

    updated Test

--- Library/Test/Test.js
+++ Library/Test/Test.js
@@ -3,13 +3,10 @@
     KUBE.LoadFactory('/Library/Test/Test',Test,['/Library/Test/Test2']);
 
     function Test(){
-        var Test2 = KUBE.Classes('/Library/Test/Test2');
+        var Test2 = KUBE.Class('/Library/Test/Test2');
         return {
             'Call':function(){
-                console.log(Test2.Get()+' from Test');
-            },
-            'Get':function(){
-                return 'Test';
+                Test2().Draw();
             }
         };
     }
