8f5fbd1bdf9956e8f7d63cdaabfbbf6825161058
Ian Reid <ian@arraystudios.com>
2015-10-29 23:15:47 -0700

    Added animations and fixed keyFrames for StyleJack
    Also, percent/ms/s added to Convert

--- src/Library/Tools/Convert.js
+++ src/Library/Tools/Convert.js
@@ -18,7 +18,9 @@
 			'deg':deg,
 			'url':url,
 			'string':string,
-            'ms': ms
+            'ms': ms,
+            's':s,
+            'percent':percent
 		};
 
 		return (ConvertCheck(_from,_originalValue) && KUBE.Is(conversionMethods[_to]) === 'function' ? conversionMethods[_to](_originalValue) : _originalValue);
@@ -46,6 +48,14 @@
 		function ms(_val){
 			return _val+'ms';
 		}
+
+        function s(_val){
+            return _val+'s';
+        }
+
+        function percent(_val){
+            return _val+'%';
+        }
 		
 		function isCSSFunction(_string){
 			return (/[^\(]*\(/.exec(_string) === null ? false : true);
